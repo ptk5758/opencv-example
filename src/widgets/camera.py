7ea8217978b9b4c17bf458fd57668604251443a9
ptk5758 <ptk725739@gmail.com>
2024-12-02 14:13:13 +0900

    image viewer widget modify

--- src/widgets/camera.py
+++ src/widgets/camera.py
@@ -1,29 +1,46 @@
 import cv2
+import threading
+import time
 from PySide6 import QtWidgets, QtCore, QtGui
+from widgets.image_viewer import ImageViewer
 class Camera(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.label = QtWidgets.QLabel("Image...")
         self.capture_button = QtWidgets.QPushButton("Capture")
         self.capture_button.clicked.connect(self.capture)
         self.camera = cv2.VideoCapture(1)
         self.camera_frame_width = self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.camera_frame_height = self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        
+        # Image Viewer
+        self.image_viewer = ImageViewer()
+
+        # Video 부분
+        self.video = Video()
+        self.video.start()
+
         self.initUI()
 
     def initUI(self):
         layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.label)
+        layout.addWidget(self.image_viewer)
         layout.addWidget(self.capture_button)
         self.setLayout(layout)
 
     @QtCore.Slot()
     def capture(self):
         state, image = self.camera.read()
-        print(state)
         if state :
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            h, w, c = image.shape
-            qt_image = QtGui.QImage(image.data, w, h, w * c, QtGui.QImage.Format.Format_RGB888)
-            pixmap = QtGui.QPixmap.fromImage(qt_image)
-            self.label.setPixmap(pixmap)
+            self.image_viewer.setImage(image)
+            # h, w, c = image.shape
+            # qt_image = QtGui.QImage(image.data, w, h, w * c, QtGui.QImage.Format.Format_RGB888)
+            # pixmap = QtGui.QPixmap.fromImage(qt_image)
+            # self.label.setPixmap(pixmap)
+
+
+class Video(threading.Thread):
+    def run(self):
+        print("시작..")
+        time.sleep(3)
+        print("종료.")
--- src/widgets/image_viewer.py
+++ src/widgets/image_viewer.py
@@ -2,12 +2,12 @@ import cv2
 from PySide6 import QtWidgets, QtGui, QtCore
 
 class ImageViewer(QtWidgets.QWidget):
-    def __init__(self, image_path=None):
+    def __init__(self, cv_image=None):
         super().__init__()
         self.label = QtWidgets.QLabel("이미지를 선택하여 주세요")
         self.initUI()
-        if image_path:
-            self.setImage(image_path)
+        if cv_image:
+            self.setImage(cv_image)
 
     def initUI(self):
         layout = QtWidgets.QVBoxLayout()
@@ -15,21 +15,21 @@ class ImageViewer(QtWidgets.QWidget):
         self.setLayout(layout)
 
     @QtCore.Slot()
-    def setImage(self, path):
-
-        """이미지를 로드하여 QLabel에 표시합니다."""
-        image = cv2.imread(path)
-        # print(image.shape) # B, G, R
-        if image is None:
-            print(f"Error: Unable to load image from path: {path}")
-            self.label.setText("이미지 로드 실패")
-            self.label.setPixmap(QtGui.QPixmap())  # 기존 이미지를 초기화
-            return
-        # print(1)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def setImage(self, cv_image):
+        # """이미지를 로드하여 QLabel에 표시합니다."""
+        # image = cv2.imread(path)
+        # # print(image.shape) # B, G, R
+        # if image is None:
+        #     print(f"Error: Unable to load image from path: {path}")
+        #     self.label.setText("이미지 로드 실패")
+        #     self.label.setPixmap(QtGui.QPixmap())  # 기존 이미지를 초기화
+        #     return
+        # # print(1)
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # print(2)
+        image = cv_image
         height, width, channel = image.shape
-        q_img = QtGui.QImage(image.data, width, height, width * 3, QtGui.QImage.Format.Format_RGB888)
+        q_img = QtGui.QImage(image.data, width, height, width * channel, QtGui.QImage.Format.Format_RGB888)
         pixmap = QtGui.QPixmap.fromImage(q_img)
         self.label.setPixmap(pixmap)
         self.label.resize(width, height)
